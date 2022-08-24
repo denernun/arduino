6937b9cbc4462c11a04358e53460af050f946429
denernun@gmail.com <Dener>
2022-08-24 08:07:43 -0300

    .

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -4,24 +4,21 @@
 
 void setup()
 {
-  Serial.begin(9600);
-  delay(2000);
-  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize LED digital pin as an output.
+  pinMode(PIN2, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(2000);
-  digitalWrite(LED_BUILTIN, LOW);
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(PIN2, HIGH);
+
+  // wait for a second
   delay(2000);
 
-  // DHT.read11(A2); //LÊ AS INFORMAÇÕES DO SENSOR
-  // Serial.print("Umidade: "); //IMPRIME O TEXTO NA SERIAL
-  // Serial.print(DHT.humidity); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO
-  // Serial.print("%"); //ESCREVE O TEXTO EM SEGUIDA
-  // Serial.print(" / Temperatura: "); //IMPRIME O TEXTO NA SERIAL
-  // Serial.print(DHT.temperature, 0); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO E REMOVE A PARTE DECIMAL
-  // Serial.println("*C"); //IMPRIME O TEXTO NA SERIAL
-  // delay(2000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(PIN2, LOW);
+
+  // wait for a second
+  delay(2000);
 }
